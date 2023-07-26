7061505441ec360246ffa3dfabb28ee5ad42ed1c
victor diaz <vdmpeniel@yahoo.es>
2023-07-25 22:42:37 -0400

    Added all required changes

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,20 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+
+        try {
+            String regex = "^0+[0-9].*";
+            if(str.matches(regex)){ return false; }
+            return Double.parseDouble(str) > 0;
+
+        } catch (Exception e){
+            System.out.println("An Exception occurred:");
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber(null));
     }
 }
